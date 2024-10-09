6f138f9e13d3e7f1ab0358fac8fce737c733a3a9
Shay Bushary <shaybush93@gmail.com>
2024-10-09 21:26:10 +0300

    setup server with express and mongoose

---
+++ server/src/app.ts
@@ -0,0 +1,32 @@
+import express, { NextFunction, Request, Response } from 'express';
+import { createServer } from 'node:http';
+import path from 'path';
+import cors from 'cors';
+import { AppConfig } from './config/app.config';
+import { AppError } from './utils/app-error.util';
+
+const app = express();
+const server = createServer(app);
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    console.log(req.method, req.originalUrl)
+    next()
+})
+
+app.get(AppConfig.apiUrl.health, (req, res) => {
+    res.send("OK");
+})
+
+app.use((err: AppError, req: Request, res: Response, _next: NextFunction) => {
+    console.error(`${req.method}:${req.originalUrl}, failed with error:${err}`);
+    res.status(err.httpCode).json({ message: err.message, title: err.name, isOperational: err.isOperational })
+});
+
+server.listen(AppConfig.port, () => {
+    console.log(`server is up on: http://localhost:${AppConfig.port}`);
+});
+
