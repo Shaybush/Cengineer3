39f907f2cda8ecaa69155bcfe07949325c4ea3d8
Netanel <128292678+NetanelSegal@users.noreply.github.com>
2024-10-20 15:13:59 +0300

    Cen 4 - docker (#3)
    * added Dockerfile + .dockerignore to client and server
    
    * added docker-compose.yaml with client, server, mongo and redis services

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: {
+		host: true, // This makes the server listen on all network interfaces
+		port: 5173,
+	},
+});
