4da57f686aa5ea91f91e4dcfb47d62999306c038
Netanel Segal <segal.netanel4@gmail.com>
2024-11-01 14:43:41 +0200

    fix after review

--- server/src/utils/functions.utils.ts
+++ server/src/utils/functions.utils.ts
@@ -1,4 +1,4 @@
-import {randomInt} from 'crypto';
+import { randomInt } from 'crypto';
 export function generateRandomDigitsCode(length: number): string {
     return randomInt(10 ** (length - 1), 10 ** length - 1).toString();
 }
