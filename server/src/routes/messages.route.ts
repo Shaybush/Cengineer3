6774d142ffdcb799d28daafcb1035bd4a55fe175
NetanelSegal <segal.netanel4@gmail.com>
2024-11-18 20:18:05 +0200

    WIP filtering route that will be replaced with socket

--- server/src/routes/messages.route.ts
+++ server/src/routes/messages.route.ts
@@ -2,16 +2,8 @@ import { Router } from 'express';
 
 const messagesRoute = Router();
 
-messagesRoute.post('/', (req, res) => {
-	res.send('Logic to send a new message');
-});
-
 messagesRoute.get('/:roomId', (req, res) => {
 	res.send('Logic to get all messages for a room');
-});
-
-messagesRoute.delete('/:id', (req, res) => {
-	res.send('Logic to delete a message');
 });
 
 export default messagesRoute;
--- server/src/routes/rooms.route.ts
+++ server/src/routes/rooms.route.ts
@@ -2,20 +2,8 @@ import { Router } from 'express';
 
 const roomsRoute = Router();
 
-roomsRoute.post('/', (req, res) => {
-	res.send('Logic to create a new room');
-});
-
 roomsRoute.get('/', (req, res) => {
 	res.send('Logic to get all rooms for the user');
-});
-
-roomsRoute.get('/:id', (req, res) => {
-	res.send('Logic to get details of a room');
-});
-
-roomsRoute.put('/:id', (req, res) => {
-	res.send('Logic to update room details');
 });
 
 roomsRoute.delete('/:id', (req, res) => {
