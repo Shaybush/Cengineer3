7522f562a65a23914e8ee487f0eda5e7126cf425
Shay Bushary <shaybush93@gmail.com>
2024-11-19 20:18:46 +0200

    input button ready without icons

--- client/src/components/input/inputBtn.tsx
+++ client/src/components/input/inputBtn.tsx
@@ -1,35 +1,39 @@
 import { FC } from 'react'
-import {} from '@fortawesome/react-fontawesome';
 
 interface inputBtnPropsModel {
   placeholder: string;
-  icon?: string;
-  buttonLabel?: string;
-  onClickHandle: () => void
+  buttonLabel: string;
+  id: string;
+  onClickHandle?: () => void;
+  btnType?: HTMLButtonElement['type'];
+  inputIcon?: string;
+  buttonIcon?: string;
+  type?: HTMLInputElement['type'];
+  name?: string;
 }
- 
-const InputBtn: FC<inputBtnPropsModel> = ({placeholder = 'Enter email here', onClickHandle}) => {
+
+const InputBtn: FC<inputBtnPropsModel> = ({buttonLabel, placeholder, id, onClickHandle, inputIcon, buttonIcon, btnType = 'button',type = 'text', name = '' }) => {
   return <div>
   <div className="mt-2 flex rounded-md shadow-sm">
     <div className="relative flex grow items-stretch focus-within:z-10">
       <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-        {/* <UsersIcon aria-hidden="true" className="h-5 w-5 text-gray-400" /> */}
+        {inputIcon ? inputIcon : ''}
       </div>
       <input
-        id="email"
-        name="email"
-        type="email"
+        id={id}
+        name={name}
+        type={type}
         placeholder={placeholder}
-        className="block w-full rounded-none rounded-l-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+        className={`block w-full rounded-none rounded-l-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-dark-blue sm:text-sm/6 ${inputIcon ? 'pl-10' : 'pl-2'}`}
       />
     </div>
     <button
-      onClick={onClickHandle}
-      type="button"
-      className="relative -ml-px inline-flex items-center gap-x-1.5 rounded-r-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+      onClick={onClickHandle ? onClickHandle : () => {}}
+      type={btnType}
+      className="relative -ml-16 z-10 inline-flex items-center gap-x-1.5 rounded-r-md px-3 py-2 text-sm font-semibold text-white ring-1 ring-inset"
     >
-      {/* <BarsArrowUpIcon aria-hidden="true" className="-ml-0.5 h-5 w-5 text-gray-400" /> */}
-      Sort
+      {buttonIcon ? buttonIcon : ''}
+      {buttonLabel}
     </button>
   </div>
 </div>
