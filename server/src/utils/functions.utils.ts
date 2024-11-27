c336ae5e62e343ead86b10d210fae75178fbbaa9
Netanel <128292678+NetanelSegal@users.noreply.github.com>
2024-11-27 22:44:51 +0200

    CEN-13 (#18)
    * created user.service.ts and fixed sendEmail function
    
    * added return types from functions, fixed redis on the way

--- server/src/utils/functions.utils.ts
+++ server/src/utils/functions.utils.ts
@@ -1,4 +1,11 @@
+import fs from 'fs';
 import { randomInt } from 'crypto';
 export function generateRandomDigitsCode(length: number): string {
-    return randomInt(10 ** (length - 1), 10 ** length - 1).toString();
+	return randomInt(10 ** (length - 1), 10 ** length - 1).toString();
+}
+
+export function readFile(path: string): Promise<string> {
+	return new Promise<string>((resolve, reject) => {
+		fs.readFile(path, 'utf8', (err, data) => (err ? reject(err) : resolve(data)));
+	});
 }
