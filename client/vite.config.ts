867894cfee1181cda3dd599641a91a9e6a913218
Shay Bushary <shaybush93@gmail.com>
2025-01-24 09:55:11 +0200

    try to deploy client #13

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
 		port: 5173,
 		open: true,
 	},
+	base: process.env.NODE_ENV === 'production' ? '/Cengineer3/' : '/',
 	build: {
 		sourcemap: true, // Enable source maps
 	},
