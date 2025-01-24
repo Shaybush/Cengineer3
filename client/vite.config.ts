24127c35e2bf50df0169723438d451dd97432b4c
Shay Bushary <shaybush93@gmail.com>
2025-01-24 09:55:25 +0200

    try to deploy client #13

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: process.env.NODE_ENV === 'production' ? '/Cengineer3/' : '/',
 	server: {
 		host: true, // This makes the server listen on all network interfaces
 		port: 5173,
 		open: true,
 	},
-	base: process.env.NODE_ENV === 'production' ? '/Cengineer3/' : '/',
 	build: {
 		sourcemap: true, // Enable source maps
 	},
